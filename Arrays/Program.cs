75f18e3431dcc0f1c9a0154d0677a5ca1013d512
taojones87@gmail.com <taojones87@gmail.com>
2019-01-29 07:12:23 -0500

    Version 3

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -6,7 +6,23 @@ namespace Arrays
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Introduction message. No user input necessary.
+            Console.WriteLine("Welcome to the array program. The program will now list each element value in the array.");
+            Console.WriteLine("");
+
+            // Create an array with 25 elements.
+            int[] array1 = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25 };
+
+            // Display each element in the array.
+            foreach (int i in array1)
+            {
+                Console.WriteLine("Element Value = " + i);
+            }
+
+            // Closing message and instructions.
+                Console.WriteLine("");
+                Console.WriteLine("Press any key to close the program...");
+                Console.ReadKey(true);            
         }
     }
 }
